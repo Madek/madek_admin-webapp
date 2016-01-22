747c20e139e8ff24110ad66fa9e6430955d44568
Michał Podlecki <m.podlecki@op.pl>
2016-01-22 10:19:03 +0100

    set schema format to sql on test env

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -31,6 +31,11 @@ Rails.application.configure do
   # ActionMailer::Base.deliveries array.
   config.action_mailer.delivery_method = :test
 
+  # Use SQL instead of Active Record's schema dumper when creating the test database.
+  # This is necessary if your schema can't be completely dumped by the schema dumper,
+  # like if you have constraints or database-specific column types
+  config.active_record.schema_format = :sql
+
   # Randomize the order test cases are executed.
   config.active_support.test_order = :random
 
