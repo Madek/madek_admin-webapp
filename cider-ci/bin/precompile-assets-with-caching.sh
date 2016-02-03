1f27a40a210d38e27fb183fce998184b01c4bba8
Michał Podlecki <m.podlecki@op.pl>
2016-02-03 14:15:58 +0100

    Adjustments for public/admin/assets path

--- cider-ci/bin/precompile-assets-with-caching.sh
+++ cider-ci/bin/precompile-assets-with-caching.sh
@@ -8,6 +8,9 @@ if [ -d "$ASSETS_CACHE_DIR" ]; then
   echo "assets cache exists, just linking..."
 else
   bundle exec rake assets:precompile
-  mv public/assets "${ASSETS_CACHE_DIR}"
+  mv public/admin/assets "${ASSETS_CACHE_DIR}"
 fi
-ln -s "$ASSETS_CACHE_DIR" public/assets
+if [ ! -d public/admin ]; then
+  mkdir public/admin
+fi
+ln -s "$ASSETS_CACHE_DIR" public/admin/assets
