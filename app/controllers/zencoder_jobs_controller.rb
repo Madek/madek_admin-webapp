6792425e98e40236c9cba246812d821ca3d0893f
Michał Podlecki <m.podlecki@op.pl>
2016-10-21 08:32:33 +0200

    display progress of processed files with Zencoder

--- app/controllers/zencoder_jobs_controller.rb
+++ app/controllers/zencoder_jobs_controller.rb
@@ -1,5 +1,6 @@
 class ZencoderJobsController < ApplicationController
   def show
     @zencoder_job = ZencoderJob.find(params[:id])
+    @zencoder_job.fetch_progress if @zencoder_job.submitted?
   end
 end
