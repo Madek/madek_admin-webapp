50b04eb7dbe7996fadf9612004999db5c4e74bdf
Michał Podlecki <m.podlecki@op.pl>
2021-06-02 11:35:26 +0200

    feat: add meta data filtering by meta key

--- app/controllers/meta_datums_controller.rb
+++ app/controllers/meta_datums_controller.rb
@@ -10,15 +10,16 @@ class MetaDatumsController < ApplicationController
   private
 
   def filter
-    if (search_term = params[:search_term]).present?
+    if (search_term = params[:search_term]&.strip).present?
       filter_method = {
         id: :with_id,
         string: :with_string,
         media_entry_id: :of_media_entry,
-        collection_id: :of_collection
-      }[params[:search_by].to_sym]
+        collection_id: :of_collection,
+        meta_key_id: :of_meta_key
+      }.fetch(params[:search_by].to_sym)
 
-      @meta_datums = @meta_datums.send(filter_method, search_term)
+      @meta_datums = @meta_datums.public_send(filter_method, search_term)
     end
 
     if (type = params[:type]).present?
