9777adc0aae9c9aa4aad1f386e22d5976931e9a2
uvb <urs.vanbinsbergen@zhdk.ch>
2023-11-13 11:43:28 +0100

    show distinct group names

--- app/helpers/groups_helper.rb
+++ app/helpers/groups_helper.rb
@@ -1,10 +1,18 @@
 module GroupsHelper
 
+  def group_name(group)
+    if group.institutional_name and not group.institutional_name.empty?
+      "#{group.name} (#{group.institutional_name})"
+    else
+      group.name
+    end
+  end
+
   def group_links(groups)
     capture_haml do
       content_tag('ul') do
         groups.order(:name).map do |group|
-          concat content_tag('li') { concat link_to group.name, group_path(group) }
+          concat content_tag('li') { link_to group_name(group), group_path(group) }
         end
       end
     end
