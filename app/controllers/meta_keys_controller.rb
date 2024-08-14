5ed01069f8c8e6695697b27ac41355713deb8d91
uvb <urs.vanbinsbergen@zhdk.ch>
2024-08-14 17:34:54 +0200

    feat: configurable keyword selection type

--- app/controllers/meta_keys_controller.rb
+++ app/controllers/meta_keys_controller.rb
@@ -102,6 +102,8 @@ class MetaKeysController < ApplicationController
         :is_enabled_for_media_entries,
         :is_enabled_for_collections,
         :allowed_rdf_class,
+        :multiple_selection,
+        :selection_field_type,
         {
           allowed_people_subtypes: []
         }.merge(localized_field_params)
