0ddbf5ee3768736c3fdd67de24afe2a1c05a9a10
Matus Kmit <matuskmit1@gmail.com>
2024-12-19 11:40:13 +0100

    feat: password reset
    //ci

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -336,7 +336,6 @@ describe UsersController do
         {
           login: Faker::Internet.user_name,
           email: Faker::Internet.email,
-          password: Faker::Internet.password,
           person_id: create(:person).id
         }
       end
@@ -385,7 +384,6 @@ describe UsersController do
         {
           login: Faker::Internet.user_name,
           email: Faker::Internet.email,
-          password: Faker::Internet.password,
           first_name: Faker::Name.first_name,
           last_name: Faker::Name.last_name
         }
