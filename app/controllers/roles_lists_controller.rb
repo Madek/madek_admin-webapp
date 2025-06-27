ab4dbf454c71055fcf290e230155a4e06b205c15
Matus Kmit <matuskmit1@gmail.com>
2025-06-27 12:39:54 +0200

    roles lists crud

---
+++ app/controllers/roles_lists_controller.rb
@@ -0,0 +1,42 @@
+class RolesListsController < ApplicationController
+  def index
+    @roles_lists = RolesList
+      .filter_by(filter_value(:term, nil))
+      .sorted
+      .page(page_params)
+      .per(16)
+  end
+
+  def new
+    @roles_list = RolesList.new
+  end
+
+  def create
+    roles_list = RolesList.new(roles_list_params)
+    roles_list.save!
+    respond_with roles_list, location: roles_lists_path
+  end
+
+  def edit
+    @roles_list = RolesList.find(params[:id])
+  end
+
+  def update
+    roles_list = RolesList.find(params[:id])
+    roles_list.update!(roles_list_params)
+    respond_with roles_list, location: roles_lists_path
+  end
+
+  def destroy
+    roles_list = RolesList.find(params[:id])
+    roles_list.destroy!
+    respond_with roles_list, location: roles_lists_path
+  end
+
+  private
+
+  def roles_list_params
+    params.require(:roles_list).permit!
+  end
+
+end
