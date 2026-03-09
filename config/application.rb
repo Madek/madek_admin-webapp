94cc86b0783a23e52f12b617fbe4b91b23ae1206
mradl <manuel.radl@zhdk.ch>
2026-03-09 14:09:43 +0100

    chore: upgrade to rails 8.0

--- config/application.rb
+++ config/application.rb
@@ -22,7 +22,7 @@ Bundler.require(*Rails.groups)
 module MadekAdmin
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 7.2
+    config.load_defaults 8.0
 
     config.responders.flash_keys = [ :success, :error ]
 
