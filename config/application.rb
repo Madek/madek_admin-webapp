5b172682e4c47ae3da97d906f52a9f9402c27c36
mradl <manuel.radl@zhdk.ch>
2026-03-31 17:30:05 +0200

    chore: rails 8.0 upgrade

--- config/application.rb
+++ config/application.rb
@@ -22,7 +22,8 @@ Bundler.require(*Rails.groups)
 module MadekAdmin
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 7.2
+    config.load_defaults 8.0
+    config.active_support.to_time_preserves_timezone = :zone
 
     config.responders.flash_keys = [ :success, :error ]
 
